72ed02837c0f20d28a748ed6a8a17acc5f8f869d
Evgeniy-Developer-droid <evgeniy.python.developer@gmail.com>
2023-05-11 19:09:58 +0300

    debug

--- server/workers_app/mixins.py
+++ server/workers_app/mixins.py
@@ -1,10 +1,15 @@
 from django.http.response import JsonResponse
 from rest_framework.status import HTTP_403_FORBIDDEN
 from django.conf import settings
+import sys
+sys.stdout.flush()
+
 
 class SecurityCodeMixin():
 
     def dispatch(self, request, *args, **kwargs):
+        print(request.META.get("SECRET_CODE", "_"), flush=True)
+        print(settings.SECRET_WORKER_KEY, flush=True)
         if request.META.get("SECRET_CODE", "_") == settings.SECRET_WORKER_KEY:
             return super().dispatch(request, *args, **kwargs)
         return JsonResponse({"error": "Access denied."}, status=403)
